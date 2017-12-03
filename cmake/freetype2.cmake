6ae51a577b9c38847bf96f52335435f57ae91b8e
Tiaan Louw <tiaanl@gmail.com>
2017-12-03 02:14:29 +0100

    Fix imports

--- cmake/freetype2.cmake
+++ cmake/freetype2.cmake
@@ -5,7 +5,11 @@ if(NOT FREETYPE2_ROOT_DIR)
 endif()
 
 add_subdirectory("${FREETYPE2_ROOT_DIR}" "freetype2")
-set_property(TARGET "freetype" PROPERTY FOLDER "third_party/freetype2")
-set_property(TARGET "dist" PROPERTY FOLDER "third_party/freetype2")
+
+set_property(TARGET freetype PROPERTY FOLDER third_party/freetype2)
+set_property(TARGET dist PROPERTY FOLDER third_party/freetype2)
 
 target_include_directories(freetype PUBLIC "${freetype_SOURCE_DIR}/include")
+target_include_directories(freetype PRIVATE "${zlib_BINARY_DIR}")
+
+target_compile_definitions(freetype PRIVATE "-D_CRT_SECURE_NO_WARNINGS")
