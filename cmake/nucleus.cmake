8b497bb7c23b816a16a551c1a4cc370092d2a305
Tiaan Louw <tiaan.louw@olx.com>
2017-12-14 15:16:35 +0100

    Add cmake include guard

--- cmake/nucleus.cmake
+++ cmake/nucleus.cmake
@@ -1,5 +1,5 @@
 
-set(NUCLEUS_ROOT_DIR "" CACHE PATH "Nucleus source root directory")
+set(NUCLEUS_ROOT_DIR "../nucleus" CACHE PATH "Nucleus source root directory")
 if(NOT NUCLEUS_ROOT_DIR)
     message(FATAL_ERROR "Nucleus not found. Please set NUCLEUS_ROOT_DIR.")
 endif()
