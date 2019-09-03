7d1ce533e8c0d5f3c939c784e68b07e9b31564f4
Tiaan Louw <tiaan.louw@olx.com>
2019-09-03 18:46:17 +0200

    Add color stream printing

--- include/canvas/Utils/Color.h
+++ include/canvas/Utils/Color.h
@@ -4,6 +4,8 @@
 
 #include "nucleus/Types.h"
 
+#include <ostream>
+
 namespace ca {
 
 struct Color {
@@ -20,6 +22,11 @@ struct Color {
   F32 a;
 
   Color(F32 r, F32 g, F32 b, F32 a = 1.0f) : r{r}, g{g}, b{b}, a{a} {}
+};
+
+inline std::ostream& operator<<(std::ostream& os, const Color& value) {
+  os << "{" << value.r << ", " << value.g << ", " << value.b << ", " << value.a << "}";
+  return os;
 };
 
 }  // namespace ca
