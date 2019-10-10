c2b68244f4025bd2b76237b8d7a4dd7c309027b9
Tiaan Louw <tiaanl@gmail.com>
2019-10-10 23:59:39 +0200

    Add some math for Vec2

--- include/canvas/Math/Vec2.h
+++ include/canvas/Math/Vec2.h
@@ -1,6 +1,7 @@
 #ifndef CANVAS_MATH_VEC2_H_
 #define CANVAS_MATH_VEC2_H_
 
+#include "canvas/Math/Common.h"
 #include "nucleus/Types.h"
 
 #include <ostream>
@@ -62,6 +63,26 @@ inline Vec2 operator/(const Vec2& left, F32 right) {
 
 inline Vec2 operator/(const Vec2& left, const Vec2& right) {
   return {left.x / right.x, left.y / right.y};
+}
+
+inline F32 dotProduct(const Vec2& left, const Vec2& right) {
+  return left.x * right.x + left.y * right.y;
+}
+
+inline F32 lengthSquared(const Vec2& v) {
+  return dotProduct(v, v);
+}
+
+inline F32 length(const Vec2& v) {
+  return squareRoot(lengthSquared(v));
+}
+
+inline Vec2 normalize(const Vec2& v) {
+  return v * (1.0f / length(v));
+}
+
+inline Vec2 lerp(const Vec2& a, const Vec2& b, F32 t) {
+  return a + (b - a) * t;
 }
 
 inline std::ostream& operator<<(std::ostream& os, const Vec2& value) {
