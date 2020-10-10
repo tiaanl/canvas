b046f14cf24075d56bf86dc25cb200f2f6e25b4d
Tiaan Louw <tiaanl@gmail.com>
2020-10-10 22:31:38 +0200

    Fix tests.

--- tests/Renderer/VertexDefinitionTests.cpp
+++ tests/Renderer/VertexDefinitionTests.cpp
@@ -4,38 +4,20 @@
 
 namespace ca {
 
-TEST_CASE("create empty vertex definition") {
-  VertexDefinition vd;
-  CHECK(vd.getAttributes().isEmpty());
-}
-
 TEST_CASE("add single vertex attribute") {
   VertexDefinition vd;
-  vd.addAttribute(ComponentType::Float32, ComponentCount::Three, "position");
-  vd.addAttribute(ComponentType::Unsigned32, ComponentCount::Two, "something");
+  vd.addAttribute(ComponentType::Float32, ComponentCount::Three);
+  vd.addAttribute(ComponentType::Unsigned32, ComponentCount::Two);
 
-  auto& attributes = vd.getAttributes();
+  auto attribute = vd.begin();
 
-  CHECK(attributes[0].getType() == ComponentType::Float32);
-  CHECK(attributes[0].getCount() == ComponentCount::Three);
-  CHECK(attributes[0].getName() == "position");
+  CHECK(attribute->getType() == ComponentType::Float32);
+  CHECK(attribute->getCount() == ComponentCount::Three);
 
-  CHECK(attributes[1].getType() == ComponentType::Unsigned32);
-  CHECK(attributes[1].getCount() == ComponentCount::Two);
-  CHECK(attributes[1].getName() == "something");
+  ++attribute;
+
+  CHECK(attribute->getType() == ComponentType::Unsigned32);
+  CHECK(attribute->getCount() == ComponentCount::Two);
 }
-
-#if 0
-TEST_CASE("build correct shader source") {
-  VertexDefinition vd;
-  vd.addAttribute(ComponentType::Float32, ComponentCount::Three, "position");
-  vd.addAttribute(ComponentType::Unsigned32, ComponentCount::Two, "something");
-
-  nu::DynamicString text = vd.buildShaderSource();
-  LOG(Info) << text.getData();
-  CHECK(text.compare("layout(location = 0) in vec3 in_position;\nlayout(location = 1) in vec2 "
-                     "in_something;\n") == 0);
-}
-#endif  // 0
 
 }  // namespace ca
